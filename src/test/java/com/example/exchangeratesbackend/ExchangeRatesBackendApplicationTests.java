21ba391c5db94ead385452ec05cc52b216c26838
KarolPatko <karolpatko@gmail.com>
2020-11-21 22:18:44 +0100

    Initialize repository

---
+++ src/test/java/com/example/exchangeratesbackend/ExchangeRatesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangeratesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRatesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
