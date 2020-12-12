fd43c955003efb96f777626f7b3f0ae5618c0266
estymator <pawel.chelmecki55@gmail.com>
2020-12-12 17:45:53 +0100

    added get favourite

--- src/main/java/com/example/exchangeratesbackend/repository/UserRepository.java
+++ src/main/java/com/example/exchangeratesbackend/repository/UserRepository.java
@@ -8,8 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
     User getByLogin(String login);
     User getById(Long Id);
+
+    Optional<User> findUserByLogin(String login);
 }
