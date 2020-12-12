5869a698eed1797432993596ad33a44a698edb57
estymator <pawel.chelmecki55@gmail.com>
2020-12-12 18:28:07 +0100

    add post/delete method for favourite entity

--- src/main/java/com/example/exchangeratesbackend/controller/FavouriteController.java
+++ src/main/java/com/example/exchangeratesbackend/controller/FavouriteController.java
@@ -1,13 +1,12 @@
 package com.example.exchangeratesbackend.controller;
 
+import com.example.exchangeratesbackend.dto.FavouriteDto;
 import com.example.exchangeratesbackend.service.FavouriteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/favourite/currency")
@@ -15,9 +14,16 @@ public class FavouriteController {
     @Autowired
     FavouriteService favouriteService;
 
-    @GetMapping
-    public ArrayList<Long> getAllFavouriteCurrencies(@RequestHeader("Authorization") String authorization){
+    @GetMapping(path = "/currencyId")
+    public List<Long> getAllFavouriteCurrencies(@RequestHeader("Authorization") String authorization){
         return favouriteService.getAllFavouriteCurrencies(authorization);
+    }
+
+    @PostMapping("/{currencyId}")
+    public void addOrDeleteFavourite(@RequestHeader("Authorization") String authorization,
+                                       @PathVariable("currencyId") Long currencyId,
+                                       @RequestBody FavouriteDto favouriteDto){
+        favouriteService.addOrDeleteFavourite(authorization, favouriteDto, currencyId);
     }
 
 }
--- src/main/java/com/example/exchangeratesbackend/repository/FavouriteRepository.java
+++ src/main/java/com/example/exchangeratesbackend/repository/FavouriteRepository.java
@@ -1,14 +1,20 @@
 package com.example.exchangeratesbackend.repository;
 
+import com.example.exchangeratesbackend.dto.FavouriteCurrencyIdDto;
 import com.example.exchangeratesbackend.entitie.Favourite;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FavouriteRepository extends JpaRepository<Favourite, Long> {
-    @Query("SELECT f.currencyId FROM Favourite f where f.userId=:usId")
-    ArrayList<Long> getCurrencyIdByUserId(Long userId);
+
+
+    List<Long> findCurrencyIdByUserId(Long userId);
+
+    Optional<Favourite> findByCurrencyIdAndAndUserId(Long currencyId, Long userId);
 }
