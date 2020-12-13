f66d3f499c03bee5d29687baa09e747544dd56df
KarolPatko <karolpatko@gmail.com>
2020-12-13 17:17:33 +0100

    add id to get currency rates

--- src/main/java/com/example/exchangeratesbackend/entitie/projection/CurrencyValuesProjection.java
+++ src/main/java/com/example/exchangeratesbackend/entitie/projection/CurrencyValuesProjection.java
@@ -3,6 +3,7 @@ package com.example.exchangeratesbackend.entitie.projection;
 import java.time.LocalDate;
 
 public interface CurrencyValuesProjection {
+    Long getId();
     Double getValue();
     LocalDate getDate();
 }
--- src/main/java/com/example/exchangeratesbackend/repository/RateRepository.java
+++ src/main/java/com/example/exchangeratesbackend/repository/RateRepository.java
@@ -10,7 +10,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface RateRepository extends JpaRepository<Rate, Long> {
-    @Query("SELECT r.value as value, r.date as date FROM Rate r WHERE r.currencyId=:currId")
+    @Query("SELECT r.value as value, r.date as date, r.id as id FROM Rate r WHERE r.currencyId=:currId")
     ArrayList<CurrencyValuesProjection> findAllCurrencyValuesProjectedByCurrencyId(@Param("currId") long currencyId);
     Rate findByCurrencyIdAndDate(Long currencyId, LocalDate date);
 
