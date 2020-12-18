0e0f3faf66537453accddb71444cfcd538abc667
KarolPatko <karolpatko@gmail.com>
2020-12-18 18:01:50 +0100

    Add rates

--- src/main/java/com/example/exchangeratesbackend/DataLoader.java
+++ src/main/java/com/example/exchangeratesbackend/DataLoader.java
@@ -33,6 +33,10 @@ public class DataLoader {
     }
 
     private void loadRate(){
+        Currency currency1 = new Currency();
+        currency1.setName("BTC");
+        currencyRepository.save(currency1);
+
         Currency currency2 = new Currency();
         currency2.setName("GBP");
         currencyRepository.save(currency2);
@@ -53,65 +57,131 @@ public class DataLoader {
         currency6.setName("JPY");
         currencyRepository.save(currency6);
 
-        Currency currency7 = new Currency();
-        currency7.setName("BTX");
-        currencyRepository.save(currency7);
+        Rate rate11 = new Rate();
+        LocalDate localDate11 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate11.setDate(localDate11);
+        rate11.setCurrencyId(currency1.getId());
+        rate11.setValue(new Double(100));
+        rateRepository.save(rate11);
 
-        Rate rate3 = new Rate();
-        LocalDate localDate3 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate3.setDate(localDate3);
-        rate3.setCurrencyId(currency2.getId());
-        rate3.setValue(new Double(2));
-        rateRepository.save(rate3);
+        Rate rate12 = new Rate();
+        LocalDate localDate12 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate12.setDate(localDate12);
+        rate12.setCurrencyId(currency1.getId());
+        rate12.setValue(new Double(101));
+        rateRepository.save(rate12);
 
-        Rate rate4 = new Rate();
-        LocalDate localDate4 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate4.setDate(localDate4);
-        rate4.setCurrencyId(currency3.getId());
-        rate4.setValue(new Double(1.5));
-        rateRepository.save(rate4);
+        Rate rate13 = new Rate();
+        LocalDate localDate13 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate13.setDate(localDate13);
+        rate13.setCurrencyId(currency1.getId());
+        rate13.setValue(new Double(99));
+        rateRepository.save(rate13);
 
-        Rate rate6 = new Rate();
-        LocalDate localDate6 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate6.setDate(localDate6);
-        rate6.setCurrencyId(currency3.getId());
-        rate6.setValue(new Double(2));
-        rateRepository.save(rate6);
+        Rate rate21 = new Rate();
+        LocalDate localDate21 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate21.setDate(localDate21);
+        rate21.setCurrencyId(currency2.getId());
+        rate21.setValue(new Double(2));
+        rateRepository.save(rate21);
 
-        Rate rate7 = new Rate();
-        LocalDate localDate7 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate7.setDate(localDate7);
-        rate7.setCurrencyId(currency3.getId());
-        rate7.setValue(new Double(3));
-        rateRepository.save(rate7);
+        Rate rate22 = new Rate();
+        LocalDate localDate22 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate22.setDate(localDate22);
+        rate22.setCurrencyId(currency2.getId());
+        rate22.setValue(new Double(2));
+        rateRepository.save(rate22);
 
-        Rate rate5 = new Rate();
-        LocalDate localDate5 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate5.setDate(localDate5);
-        rate5.setCurrencyId(currency4.getId());
-        rate5.setValue(new Double(100));
-        rateRepository.save(rate5);
+        Rate rate23 = new Rate();
+        LocalDate localDate23 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate23.setDate(localDate23);
+        rate23.setCurrencyId(currency2.getId());
+        rate23.setValue(new Double(1.8));
+        rateRepository.save(rate23);
 
-        Rate rate8 = new Rate();
-        LocalDate localDate8 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate8.setDate(localDate8);
-        rate8.setCurrencyId(currency5.getId());
-        rate8.setValue(new Double(2));
-        rateRepository.save(rate8);
+        Rate rate31 = new Rate();
+        LocalDate localDate31 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate31.setDate(localDate31);
+        rate31.setCurrencyId(currency3.getId());
+        rate31.setValue(new Double(1));
+        rateRepository.save(rate31);
 
-        Rate rate9 = new Rate();
-        LocalDate localDate9 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate9.setDate(localDate9);
-        rate9.setCurrencyId(currency6.getId());
-        rate9.setValue(new Double(100));
-        rateRepository.save(rate9);
+        Rate rate32 = new Rate();
+        LocalDate localDate32 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate32.setDate(localDate32);
+        rate32.setCurrencyId(currency3.getId());
+        rate32.setValue(new Double(2));
+        rateRepository.save(rate32);
 
-        Rate rate10 = new Rate();
-        LocalDate localDate10 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        rate10.setDate(localDate10);
-        rate10.setCurrencyId(currency7.getId());
-        rate10.setValue(new Double(100));
-        rateRepository.save(rate10);
+        Rate rate33 = new Rate();
+        LocalDate localDate33 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate33.setDate(localDate33);
+        rate33.setCurrencyId(currency3.getId());
+        rate33.setValue(new Double(4));
+        rateRepository.save(rate33);
+
+        Rate rate41 = new Rate();
+        LocalDate localDate41 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate41.setDate(localDate41);
+        rate41.setCurrencyId(currency4.getId());
+        rate41.setValue(new Double(1.5));
+        rateRepository.save(rate41);
+
+        Rate rate42 = new Rate();
+        LocalDate localDate42 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate42.setDate(localDate42);
+        rate42.setCurrencyId(currency4.getId());
+        rate42.setValue(new Double(1));
+        rateRepository.save(rate41);
+
+        Rate rate43 = new Rate();
+        LocalDate localDate43 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate43.setDate(localDate43);
+        rate43.setCurrencyId(currency4.getId());
+        rate43.setValue(new Double(0.5));
+        rateRepository.save(rate43);
+
+        Rate rate51 = new Rate();
+        LocalDate localDate51 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate51.setDate(localDate51);
+        rate51.setCurrencyId(currency5.getId());
+        rate51.setValue(new Double(100));
+        rateRepository.save(rate51);
+
+        Rate rate52 = new Rate();
+        LocalDate localDate52 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate52.setDate(localDate52);
+        rate52.setCurrencyId(currency5.getId());
+        rate52.setValue(new Double(97));
+        rateRepository.save(rate52);
+
+        Rate rate53 = new Rate();
+        LocalDate localDate53 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate53.setDate(localDate53);
+        rate53.setCurrencyId(currency5.getId());
+        rate53.setValue(new Double(90));
+        rateRepository.save(rate53);
+
+        Rate rate61 = new Rate();
+        LocalDate localDate61 = LocalDate.parse("2020-12-14", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate61.setDate(localDate61);
+        rate61.setCurrencyId(currency6.getId());
+        rate61.setValue(new Double(2));
+        rateRepository.save(rate61);
+
+        Rate rate62 = new Rate();
+        LocalDate localDate62 = LocalDate.parse("2020-12-15", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate62.setDate(localDate62);
+        rate62.setCurrencyId(currency6.getId());
+        rate62.setValue(new Double(1.7));
+        rateRepository.save(rate62);
+
+        Rate rate63 = new Rate();
+        LocalDate localDate63 = LocalDate.parse("2020-12-16", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        rate63.setDate(localDate63);
+        rate63.setCurrencyId(currency6.getId());
+        rate63.setValue(new Double(2));
+        rateRepository.save(rate63);
     }
 
     private void loadUser(){
